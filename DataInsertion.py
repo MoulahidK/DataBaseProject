b0c9e9c36b0ee867ad06b8f5aa1291ece49032fb
hyungkawmld <moulahidkawtar@email.com>
2022-03-04 16:55:43 +0100

    Add the script for data insertion into the database

---
+++ DataInsertion.py
@@ -0,0 +1,47 @@
+
+"""
+Created on Thu Feb 12 2022
+
+@author: Kawtar
+"""
+import DataImport, DataBaseConnectionCreation
+from   DataBaseConnectionCreation import Error, mysql, connection
+from   DataImport import emported_data, winners_emported_data
+try:
+
+    if connection.is_connected():
+        cursor = connection.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("You're connected to database: ", record)
+
+        # Checking if the table exists
+        cursor.execute('DROP TABLE IF EXISTS `match2`;')
+        cursor.execute('DROP TABLE IF EXISTS `winners`;')
+        print('Creating tables....')
+
+		# Creating the table using an SQL statement
+        cursor.execute("CREATE TABLE  `LOL`.`match2` (`_id` varchar(15), `temps` int(2), `blue_ward_placed` int(1), `blue_ward_kill` int(1), `blue_level_up` int(1), `blue_champion_kill` int(1), `blue_champion_assist` int(2), `blue_champion_special_kill` int(1), `blue_elite_monster_kill` int(1), `blue_building_kill` int(1), `blue_turret_plate_destroyed` int(1), `blue_gold_earned` int(4), `blue_minions_killed` int(2), `red_ward_placed` int(1), `red_ward_kill` int(1), `red_level_up` int(1), `red_champion_kill` int(1), `red_champion_assist` int(2), `red_champion_special_kill` int(1), `red_elite_monster_kill` int(1), `red_building_kill` int(1), `red_turret_plate_destroyed` int(1), `red_gold_earned` int(4), `red_minions_killed` int(2)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;")
+        cursor.execute("CREATE TABLE  `LOL`.`winners`( `_id` varchar(15), `team` varchar(15)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;")
+        print("Tables are created....")
+
+        # Loop through the Dataframe of the General Data
+        for i,row in emported_data.iterrows(): # Inserting each Dataframe row to the table row
+            sql = "INSERT INTO `LOL`.`match2` VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+            cursor.execute(sql,tuple(row))
+        	#The connection is not auto committed by default, so we must commit to save our change
+            connection.commit()
+            print("General Record inserted")
+
+        # Loop through the Dataframe of the Winners Data
+        for i,row in winners_emported_data.iterrows(): # Inserting each Dataframe row to the table row
+            sql = "INSERT INTO `LOL`.`winners` VALUES (%s, %s)"
+            cursor.execute(sql,tuple(row))
+        	#The connection is not auto committed by default, so we must commit to save our change
+            connection.commit()
+            print("Winners Record inserted")
+        print("DONE")
+
+except Error as e:
+            print("Error while connecting to MySQL", e)
+
